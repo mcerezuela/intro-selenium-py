c8d3d24ea6a70acaa5f8ce2f83454e2eafc5e120
marcusalten <marcus.cerezuela@alten.es>
2021-03-30 14:24:35 +0200

    Added Chrome driver path, added action and element searches

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,9 +8,11 @@ import selenium.webdriver
 
 @pytest.fixture
 def browser():
+  #Drivers
+  CHROME_DRIVER_PATH = '../chromedriver.exe'
 
   # Initialize the ChromeDriver instance
-  b = selenium.webdriver.Chrome()
+  b = selenium.webdriver.Chrome(CHROME_DRIVER_PATH)
 
   # Make its calls wait up to 10 seconds for elements to appear
   b.implicitly_wait(10)
--- tests/test_search.py
+++ tests/test_search.py
@@ -7,26 +7,23 @@ from pages.search import DuckDuckGoSearchPage
 
 
 def test_basic_duckduckgo_search(browser):
-    search_page = DuckDuckGoSearchPage(browser)
-    result_page = DuckDuckGoResultPage(browser)
-    PHRASE = "panda"
+  search_page = DuckDuckGoSearchPage(browser)
+  result_page = DuckDuckGoResultPage(browser)
+  PHRASE = "panda"
 
-    # Given the DuckDuckGo home page is displayed
-    search_page.load()
+  # Given the DuckDuckGo home page is displayed
+  search_page.load()
 
-    # When the user searches for "panda"
-    search_page.search(PHRASE)
+  # When the user searches for "panda"
+  search_page.search(PHRASE)
 
-    # Then the search result title contains "panda"
-    assert PHRASE in result_page.title()
+  # Then the search result title contains "panda"
+  assert PHRASE in result_page.title()
 
-    # And the search result query is "panda"
-    assert PHRASE == result_page.search_input_value()
+  # And the search result query is "panda"
+  assert PHRASE == result_page.search_input_value()
 
-    # And the search result links pertain to "panda"
-    titles = result_page.result_link_titles()
-    matches = [t for t in titles if PHRASE.lower() in t.lower()]
-    assert len(matches) > 0
-
-    # TODO: Remove this exception once the test is complete
-    raise Exception("Incomplete Test")
+  # And the search result links pertain to "panda"
+  titles = result_page.result_link_titles()
+  matches = [t for t in titles if PHRASE.lower() in t.lower()]
+  assert len(matches) > 0
